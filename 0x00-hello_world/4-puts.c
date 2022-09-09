1848b6e3244d9ccf2f5fc96b4bc16c3cad358a86
Aniekan George Edet <aniekan.g.edet@gmail.com>
2022-09-08 18:54:08 -0700

    added 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/
+/*
  * main - main block
  * Return: 0
  */
 int main()
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
