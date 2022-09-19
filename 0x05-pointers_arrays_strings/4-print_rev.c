4cf5343cdfccabcfa3e6086ec8fabbeae2c237cd
Aniekan George Edet <aniekan.g.edet@gmail.com>
2022-09-19 14:32:43 -0700

    first commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,35 +1,18 @@
 #include "main.h"
 
-
-
-/**
- *
- *  * print_rev - prints string in reverse
- *
- *   * @s: the used string reference pointer
- *
- *    * Return: 0
- *
- *     */
-
-
+/*
+* print_rev - prints string in reverse
+* @s: the used string reference pointer
+* Return: 0
+*/
 
 void print_rev(char *s)
-
 {
-
-		int i = 0;
-
-
-
-			while (s[i])
-
-						i++;
-
-				while (i--)
-
-							_putchar(s[i]);
-
-					_putchar('\n');
-
+	int i = 0;
+	
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,51 +1,26 @@
 #include "main.h"
 
-
-
-/**
- *
- *  * rev_string - prints a string in reverse
- *
- *   * @s: the string to be reversed
- *
- *    * Return: 0
- *
- *     */
-
-
+/*
+* rev_string - prints a string in reverse
+* @s: the string to be reversed
+* Return: 0
+*/
 
 void rev_string(char *s)
-
 {
+	int len, i, half;
+	char temp;
 
-		int len, i, half;
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	i = 0;
+	half = len / 2;
 
-			char temp;
-
-
-
-				for (len = 0; s[len] != '\0'; len++)
-
-							;
-
-					i = 0;
-
-						half = len / 2;
-
-
-
-							while (half--)
-
-									{
-
-												temp = s[len - i - 1];
-
-														s[len - i - 1] = s[i];
-
-																s[i] = temp;
-
-																		i++;
-
-																			}
-
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
 }
