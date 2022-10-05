62bc1f2614a8a1a7e11a50391d69264e2f74b2b3
Aniekan George Edet <aniekan.g.edet@gmail.com>
2022-10-05 11:26:13 -0700

    new comm

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 
 /**
 * _strdup - Returns a pointer to a newly-allocated space in memory
-	    containing a copy of the string given as parameter.
+*	    containing a copy of the string given as parameter.
 * @str: The string to be copied.
 *
 * Return: If str == NULL or insufficient memory is available - NULL.
@@ -30,6 +30,6 @@ char *_strdup(char *str)
 		duplicate[index] = str[index];
 	
 	duplicate[len] = '\0';
-
+	
 	return (duplicate);
 }
